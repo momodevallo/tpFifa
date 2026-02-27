5d9666afe7cbb9c1802cdb8f2b897b1d4eb2c622
Mohamed Hamila <mohamed.hamila26@etu.univ-cotedazur.fr>
2026-02-27 01:51:40 +0100

    Remplacement ancien projet par backend Spring Boot FIFA

--- backend/src/routes/cartesRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMyCards } from '../controllers/cartesController.js';
-
-const router = Router();
-
-router.get('/my-cards', getMyCards);
-
-export default router;
--- backend/src/routes/joueursRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { openPack } from '../controllers/joueursController.js';
-
-const router = Router();
-
-router.post('/open-pack', openPack);
-
-export default router;
